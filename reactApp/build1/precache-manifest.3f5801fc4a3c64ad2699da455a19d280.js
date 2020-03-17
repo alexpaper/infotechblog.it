52946fbb84b8d76e3961b956354f50c70f3be43c
unknown <alessandro.carta@gmail.com>
2020-03-17 15:53:55 +0100

    First commit

---
+++ reactApp/build1/precache-manifest.3f5801fc4a3c64ad2699da455a19d280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354c70f1d45d47d1e87701d353fe36b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "094d460c4e3907ba7be3",
+    "url": "/static/css/main.d6d62275.chunk.css"
+  },
+  {
+    "revision": "dc6925ba47a5e712d0cc",
+    "url": "/static/js/2.2f7ded05.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.2f7ded05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "094d460c4e3907ba7be3",
+    "url": "/static/js/main.f83d5af8.chunk.js"
+  },
+  {
+    "revision": "94def6acd220abfa7460",
+    "url": "/static/js/runtime-main.8abaf81a.js"
+  }
+]);
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Info Tech Blog
+
+>Info tech blog posts application.
+
+## Usage 
+Rename 'env.env' to '.env', fill the fields with your own data, in particular only the mongodb string connection field 'MONGO_URI='
+
+## Install dependencies
+```
+npm i --verbose
+
+```
+
+## Run app
+```
+node app.js
+
+```
+
+## Version 1.3.0
+
+### License: MIT
+
+#### auth @paper
